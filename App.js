b1c86eb37206e004c241ae6f3c03182eaa4853c8
JSS <choss01@oneplat.co>
2020-06-26 21:35:05 +0900

    navigation 구현

--- App.js
+++ App.js
@@ -6,81 +6,80 @@
  * @flow strict-local
  */
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Image,
-  Text,
-  StatusBar,
-} from 'react-native';
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Image,
+//   Text,
+//   StatusBar,
+// } from 'react-native';
 
-import React, {Fragment, Component} from "react";
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import {StyleSheet, View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
 
-class App extends Component{
-  render(){
-    return (
-      <View style = {{flex : 1}}>
-          <View style = {styles.container}>
-              <Image source={require('./assets/IronMan.jpg')} style = {{width:100, height: 100}}/>
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>안녕하세요 HomeScreen 테스트입니다.</Text>
+      <Button
+        title="프로필 페이지로 이동"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+function ProfileScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>안녕하세요 ProfileScreen 테스트입니다.</Text>
+    </View>
+  );
+}
 
-              <View style = {{flexDirection : 'column'}}>
-                  <View style = {{flexDirection: 'row'}}>
-                      <Text style = {styles.title}>활동연대</Text>
-                      <Text style = {styles.detail}>2000, 2010</Text>
-                  </View>
-                  <View style = {{flexDirection: 'row'}}>
-                      <Text style = {styles.title}>출생</Text>
-                      <Text style = {styles.detail}>2000, 2010</Text>
-                  </View>
-                  <View style = {{flexDirection: 'row'}}>
-                      <Text style = {styles.title}>활동연대</Text>
-                      <Text style = {styles.detail}>2000, 2010</Text>
-                  </View>
-                  <View style = {{flexDirection: 'row'}}>
-                      <Text style = {styles.title}>활동연대</Text>
-                      <Text style = {styles.detail}>2000, 2010</Text>
-                  </View>
-              </View>
-          </View>
-
-          <View style = {{width : 50, height : 50, backgroundColor : 'red'}}></View>
-          <View style = {{flex : 1, backgroundColor : 'yellow'}}>
-              <View style ={{flex : 1}}></View>
-              <View style ={{flex : 1, backgroundColor : 'skyblue'}}></View>
-              <View style = {{flex : 1, backgroundColor : 'red'}}></View>
-          </View>
-      </View>
-    );
-  }
+const App: () => React$Node = () => {
+  return (
+    //<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+     // <Text>안녕하세요 네비게이션 테스트입니다.</Text>
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
-    container : {
-      marginTop : 50,
-      marginLeft : 20,
-      flexDirection : 'row',
-      width : 500,
-      height : 200,
-    },
-    title : {
-        marginLeft: 10,
-        fontWeight: 'bold',
-        color : 'gray',
-    },
-    detail : {
-        marginLeft : 10
-    }
-
+  container: {
+    marginTop: 50,
+    marginLeft: 20,
+    flexDirection: 'row',
+    width: 500,
+    height: 200,
+  },
+  title: {
+    marginLeft: 10,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+  detail: {
+    marginLeft: 10,
+  },
 });
 
 export default App;
