8b978284c4bc7e1e1ffa03e6a3d5b08aa4afe292
JSS <choss01@oneplat.co>
2020-06-27 23:47:28 +0900

    간단하게 함수형으로 버튼 눌러서 변수 바꾸기

--- Components/Hook.js
+++ Components/Hook.js
@@ -16,47 +16,49 @@
 //   StatusBar,
 // } from 'react-native';
 
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
 import 'react-native-gesture-handler';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
-class Hook extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            name : '성식'
-        }
-    }
+// class Hook extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             name : '성식'
+//         }
+//     }
+//
+//     changeName = () =>{
+//         this.setState({
+//             name : 'kunghun'
+//         });
+//     }
+//
+//     render() {
+//         return(
+//             <View style = {{flex : 1, alignItems : 'center', justifyContent : 'center'}}>
+//                 <Text>안녕하세요 Hook테스트입니다.</Text>
+//                 <Button title = {'이름변경'} onPress={()=> this.changeName()}/>
+//                 <Text>안녕하세요 {this.state.name}님</Text>
+//             </View>
+//         );
+//     }
+// }
 
-    changeName = () =>{
-        this.setState({
-            name : 'kunghun'
-        });
-    }
 
-    render() {
-        return(
-            <View style = {{flex : 1, alignItems : 'center', justifyContent : 'center'}}>
-                <Text>안녕하세요 Hook테스트입니다.</Text>
-                <Button title = {'이름변경'} onPress={()=> this.changeName()}/>
-                <Text>안녕하세요 {this.state.name}님</Text>
-            </View>
-        );
-    }
-}
-
-/*
 const Hook: () => React$Node = () => {
+    const [name,setName] = useState('jin');
     return (
         <View style = {{flex : 1, alignItems : 'center', justifyContent : 'center'}}>
             <Text>안녕하세요 Hook테스트입니다.</Text>
+            <Button title = {'이름변경'} onPress={()=> setName('kunghun')}/>
+            <Text>안녕하세요 {name}</Text>
         </View>
 
     );
 };
-*/
 
 const styles = StyleSheet.create({
 
